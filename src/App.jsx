1b4b7f5bb978578087310997fd90222170d371fd
navjot-ai-dev <navjotsingh542419@gmail.com>
2026-07-08 16:22:48 +0530

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { FetchPhotos } from './api/mediaApi'
 
 const App = () => {
+
+  function getData(){
+    FetchPhotos('cat')
+  } 
+
   return (
     <div className='h-screen w-full bg-gray-950 text-white'>
-      App
+      <button onClick={getData}>get data</button>
       </div>
   )
 }
--- src/api/mediaApi.js
+++ src/api/mediaApi.js
@@ -5,9 +5,14 @@ const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASHED_KEY;
 const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY;
 
 
-export function fetchPhotos(){
-   const res = axios.get(`https://api.unsplash.com/search/photos`);
+export async function FetchPhotos(query,page=1,per_page=20){
+   const res = await axios.get(`https://api.unsplash.com/search/photos`,
+    {
+      params:{query,page,per_page},
+      headers:{Authorization:`Client-ID ${UNSPLASH_KEY}`}
+    }
+   );
 
-  console.log(res);
+  console.log(res.data);
   
 }
