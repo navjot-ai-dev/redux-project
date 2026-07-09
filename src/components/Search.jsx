8067951a04bccf9e6e57781bdcc18d32d32a6876
navjot-ai-dev <navjotsingh542419@gmail.com>
2026-07-09 20:17:55 +0530

    add logics in search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,31 @@
 import React, { useState } from 'react'
 
 const Search = () => {
+
+
   const [text, settext] = useState('');
+
   const search = (e) =>{
     settext(e.target.value)
   }
 
+  const dispatch = useDispa
+
   return (
     <div>
-     <form className='flex justify-between p-6 '>
+     <form onSubmit={(e) => 
+      {
+        e.preventDefault();
+        console.log('form submitted');
+        
+        settext('');
+        
+      }} className='flex bg-gray-800 p-6 '>
          <input
          required
          type="text" placeholder='Search anything...' value={text}
-         onChange={search} className=' border-2 px-4 py-2 text-xl w-2/2 mx-10 rounded-2xl ' />
-         <button className=' border-2 px-4 py-2 text-xl  rounded-2xl active:scale-75'>Search</button>
+         onChange={search} className=' border-2 px-4 py-2 text-xl w-full mx-10 rounded-xl ' />
+         <button className=' border-2 px-4 py-2 text-xl  rounded-xl active:scale-75'>Search</button>
      </form>
 
     </div>
