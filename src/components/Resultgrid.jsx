f01d811802cce49c7006a4465637dea2da9aab76
navjot-ai-dev <navjotsingh542419@gmail.com>
2026-07-11 12:42:55 +0530

    import element in resultg..

--- src/components/Resultgrid.jsx
+++ src/components/Resultgrid.jsx
@@ -1,6 +1,10 @@
 import {FetchPhotos,Fetchvideos,Fetchggif} from "../api/mediaApi";
+import {setQuery,setError,setLoading,setResults  } from "../redux/features/searchslice";
+import { useDispatch,useSelector } from "react-redux";
 
 const Resultgrid = () => {
+
+   
   return (
     <div>Resultgrid</div>
   )
