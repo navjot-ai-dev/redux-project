7049df9f58c467bafba8c4e366b168fef659aba6
navjot-ai-dev <navjotsingh542419@gmail.com>
2026-07-12 16:02:55 +0530

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-// const Navbar = () => {
-//   return (
-//     <div>
-//          <div className='flex justify-between items-center py-6 px-10 bg-blue-800'>
-//            <h2 className='font-medium text-2xl'>Media Search </h2>
-//            <div className='flex items-center gap-8'>
-//             <Link className='bg-white text-xl text-blue-950 px-3 py-1.5' to='/'>Search</Link>
-//             <Link className='bg-white text-xl text-blue-950 px-3 py-1.5' to='/collection'>Collection</Link>
-//            </div>
+const Navbar = () => {
+  return (
+    <div>
+         <div className='flex justify-between items-center py-6 px-10 bg-blue-800'>
+           <h2 className='font-medium text-2xl'>Media Search </h2>
+           <div className='flex items-center gap-8'>
+            <Link className='bg-white text-xl text-indigo-950 px-3 py-1.5' to='/'>Search</Link>
+            <Link className='bg-white text-xl text-indigo-950 px-3 py-1.5' to='/collection'>Collection</Link>
+           </div>
         
-//           </div>
+          </div>
 
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default Navbar
+export default Navbar
